52415fcd2b03177e6b93c1d185c307e2984a1e8d
Abhishek Mandal <abhishek.s.mandal@gmail.com>
2023-05-25 17:04:47 +0530

    Added server code

--- server/index.js
+++ server/index.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const express = require("express");
+const helmet = require("helmet");
+const { Server } = require("socket.io");
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000/",
+    credentials: "true",
+  },
+});
+
+app.use(helmet());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("hi");
+});
+
+io.on("connect", () => {});
+
+server.listen(4000, () => {
+  console.log("Server is running");
+});
